b756aa00b7e24279b0c1d58e4f4a2687c7aa1e01
Deepak Chouhan <deepakchouhan265@gmail.com>
2025-05-15 22:51:24 +0530

    Delete 01.py

--- 01.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
-print ("The sum is:", a+b)
-print ("The difference is:", a-b)
-print ("The product is:", a*b)
-print ("The quotient is:", a/b) 
