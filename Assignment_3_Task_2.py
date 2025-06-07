0cab9ecc47ba4ee4fffb4ca415b182bc27eaeb50
Deepak Chouhan <deepakchouhan265@gmail.com>
2025-06-07 21:49:53 +0530

    Add files via upload

---
+++ Assignment_3_Task_2.py
@@ -0,0 +1,14 @@
+from math import *
+
+num = int(input("Enter a number: "))
+
+squareroot = sqrt(num)
+print(f"The square root of {num} is {squareroot}")
+
+natural_log = log(num)
+print(f"The natural logarithm of {num} is {natural_log}")
+
+sine_value = sin(num)
+print(f"The sine of {num} is {sine_value}")
+
+print("Done")
